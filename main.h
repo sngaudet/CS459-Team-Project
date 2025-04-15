311869a24ed0494a8fbdf6813a28713b70ed559c
sngaudet <sngaudet@uwm.edu>
2025-04-15 18:38:51 -0500

    added sphere and rectangle to project

--- main.h
+++ main.h
@@ -3,20 +3,35 @@
 #include <math.h>
 #include <GL/glut.h> // (or others, depending on the system in use)
 #include <iostream>
+#include <Windows.h>
 
-static void display() {
+static GLfloat position[] = { 10.0, 10.0, 10.0, 1.0 };
+static GLdouble cpos[] = { 0.0, 10.0, 200.0 };
+
+void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glColor3f(1.0, 1.0, 1.0);
+    glTranslatef(0.0,0.0,0.0);
+    glScalef(0.3,0.2,0.05);
+    glutSolidCube(1.0);
+
+    glColor3f(1.0,0.5,1.0);
+    glTranslatef(0.0,0.0,0.0);
+    glutSolidSphere(0.1,100,100);
+
     glFlush();
 }
 
 void init(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(1100, 700);
+    glutInitWindowSize(700, 700);
     glutInitWindowPosition(100, 0);
     glutCreateWindow("Team Project");
 
     glClearColor(0.0, 0.0, 0.0, 1.0);
+    //glEnable(GL_DEPTH_TEST);
 
     glutDisplayFunc(display);
 }
