ee74782524980c2ba605ccb0a765f2d8e8b1eb63
unknown <darnomang@gmail.com>
2025-04-18 19:29:20 -0500

    moved main function def from header to cpp file

--- main.h
+++ main.h
@@ -8,42 +8,14 @@
 static GLfloat position[] = { 10.0, 10.0, 10.0, 1.0 };
 static GLdouble cpos[] = { 0.0, 10.0, 200.0 };
 
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+void display();
 
-    glColor3f(1.0, 1.0, 1.0);
-    glTranslatef(0.0,0.0,0.0);
-    glScalef(0.3,0.2,0.05);
-    glutSolidCube(1.0);
-
-    glColor3f(1.0,0.5,1.0);
-    glTranslatef(0.0,0.0,0.0);
-    glutSolidSphere(0.1,100,100);
-
-    glFlush();
-}
-
-void init(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(700, 700);
-    glutInitWindowPosition(100, 0);
-    glutCreateWindow("Team Project");
-
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    //glEnable(GL_DEPTH_TEST);
-
-    glutDisplayFunc(display);
-}
+void init(int argc, char** argv);
 
 void run() {
      glutMainLoop();
 }
 
-int main(int argc, char** argv) {
-    init(argc, argv);
-    run();
-    return 0;
-}
+int main(int argc, char** argv);
 
 
